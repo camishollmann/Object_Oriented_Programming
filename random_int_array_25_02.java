1ca46cdee59ef76ccd78fff6d98728430856b57e
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-03 20:27:11 -0300

    Addded comments on the use of "Random" class.

---
+++ random_int_array_25_02.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+
+        /* Random is a class that generates numbers from -2.147.487.648
+        to  2.147.483.647 (all possible int values).
+
+        You can limit (bound) that with:
+        n = r.nextInt(10);
+        This generates numbers in the range [0, 9]
+
+        You can also use math expressions for personalized intervals:
+        n = r.nexInt(101) - 50;
+        This generates numbers in the range [-50, 50].
+         */
+        Random r = new Random();
+        int n;
+
+        int[] array = new int[size];
+
+        for(int i = 0; i < size; i++){
+            // This will generate in the range [-5, 5]:
+            n = r.nextInt(11) - 5;
+            array[i] = n;
+            System.out.println(array[i]);
+        }
+    }
+}
