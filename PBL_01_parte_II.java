e586b84af87c83247b6662cbb8fa21f1f684054a
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-04 18:54:25 -0300

    Started part II.

---
+++ PBL_01_parte_II.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class PBLIPartII {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Type the size of the list: ");
+        int size = s.nextInt();
+
+        int[] array = new int[size];
+    }
+}
