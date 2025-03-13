94dbc31ca16dd9967c21a734b6d121e17b03766d
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-13 11:23:01 -0300

    PBL 03 started.

---
+++ PBL_03.java
@@ -0,0 +1,28 @@
+class Client {
+    private String name;
+    private double balance;
+
+    public Client(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+    public void deposit() {
+
+    }
+    public void withdrawl() {
+
+    }
+    public void printClientData() {
+
+    }
+}
+
+class Bank(){
+    public static void main(String[] args) {
+        Client c1 = new Client("Jandira Silva", 2500);
+        Client c2 = new Client("Sandra Rodrigues", 1800);
+        Client c3 = new Client ("Luciana Teixeira", 5000);
+
+        printClientData();
+    }
+}
