0e8270c2713b8e60e87d5f77200f42dd6dd6b40b
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-18 22:13:28 -0300

    Sorted colors exercise from 18/03 class finished.

---
+++ sorted_colors_18_03.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class sorted_colors_18_03 {
+    public static void main(String[] args) {
+        ArrayList<String> cores = new ArrayList<String>();
+
+        cores.add("Azul");
+        cores.add("Vermelho");
+        cores.add("Verde");
+        cores.add("Amarelo");
+
+        ArrayList<String> ordenado = new ArrayList<String>(cores);
+        Collections.sort(ordenado);
+
+        boolean loop = true;
+        while (loop) {
+            System.out.println("1 - Ordenado");
+            System.out.println("2 - Não Ordenado");
+            System.out.println("9 - Sair");
+
+            Scanner s = new Scanner(System.in);
+
+            int opcao = s.nextInt();
+
+            if (opcao == 1) {
+                for (String palavra : ordenado) {
+                    System.out.println(palavra);
+                }
+            } else if (opcao == 2) {
+                for (String core : cores) {
+                    System.out.println(core);
+                }
+            } else if (opcao == 9) {
+                loop = false;
+            }
+        }
+    }
+}
+
