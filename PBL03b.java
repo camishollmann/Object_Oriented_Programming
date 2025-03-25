7b1493a344e4ad801e694f262cd266ec6804c55f
Camilla Hollmann <camillahollmann@ufpr.br>
2025-03-25 17:46:44 -0300

    Code updated.

--- PBL03b.java
+++ PBL03b.java
@@ -8,14 +8,14 @@ class Usuario {
 class PBl03b{
     public static void main(String args[]) {
         boolean loop = true;
+        String usuarios[] = new String[100]; // Deve ser declarado fora para nao ser perdido.
+
         while (loop) {
             System.out.println("Bem-vindo!\t");
             System.out.println("1 - Autenticar\t");
             System.out.println("2 - Criar usuario\t");
             System.out.println("3 - Sair\t");
-            
-            String usuarios[] = new String[100];
-            
+
             Scanner t = new Scanner(System.in);
             int opcao = t.nextInt();
 
@@ -31,18 +31,18 @@ class PBl03b{
                  senha = t.next();
 
                  int i = 0;
-                 while (i <= 100) {
-                     if (usuarios[i] != null && i <= 100 - 2) {
-                         if (nome == usuarios[i] && senha == usuarios[i + 1]) {
-                             System.out.println("Seja bem-vindo!");
-                             usuario_existe = true;
-                             loop = false;
-                         } else {
-                             usuario_existe = false;
-                             System.out.println("Usuario ou senha incorretos.");
-                         }
-                         i++;
+                 boolean autenticado = false;
+                 while (i <= 100 - 2 && usuarios[i] != null) {
+                     if (nome.equals(usuarios[i]) && senha.equals(usuarios[i + 1])) {
+                         System.out.println("Seja bem-vindo!");
+                         usuario_existe = true;
+                         loop = false;
                      }
+                 i += 2; // Avanca para o proximo par [nome, senha].
+                 }
+
+                 if (!usuario_existe){
+                     System.out.println("Usuario ou senha incorretos.");
                  }
              } else if (opcao == 2) {
                  System.out.println("Digite o nome de usuario: ");
