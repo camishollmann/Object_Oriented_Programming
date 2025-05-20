747e4000f89ab6415746330eba12f09c97474b4a
Camilla Hollmann <camilla.hollmann@pucpr.edu.br>
2025-05-19 22:14:22 -0300

    Exercise 3 started.

---
+++ RA2/revisaoP2-19_05_2025/Banco_Slide05/Conta.java
@@ -0,0 +1,20 @@
+public class Conta {
+    String nomeTitular;
+    String numeroConta;
+    double saldo;
+    String dataAbertura;
+
+    public Conta(String nomeTitular, String numeroConta, double saldo){
+        this.nomeTitular = nomeTitular;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public boolean sacar(double valor){
+        
+    }
+
+    public boolean depositar(double valor){
+
+    }
+}
---
+++ RA2/revisaoP2-19_05_2025/Banco_Slide05/Operacoes.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList; 
+
+public class Operacoes {
+    public static void main(String[] args){
+        
+    }
+}
